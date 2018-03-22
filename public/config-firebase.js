d771eeab3340b6987f26d0ffa2fbf99f66349eae
mathildebuenerd <mathilde.buenerd@gmail.com>
2018-03-22 15:44:14 +0100

    ajout du fichier de config de firebase qui n'était pas sur le git

---
+++ public/config-firebase.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mathi on 27/02/2018.
+ */
+
+let config = {
+    apiKey: "AIzaSyBDaIIzyikvy0f6DfWLMXgK3azDv-RD3GQ",
+    authDomain: "shiffman-example.firebaseapp.com",
+    databaseURL: "https://shiffman-example.firebaseio.com",
+    projectId: "shiffman-example",
+    storageBucket: "shiffman-example.appspot.com",
+    messagingSenderId: "231741872198"
+};
